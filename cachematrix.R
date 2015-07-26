1ada0059f84328bf33dc43122b5c9e8d290a983a
normanenriquez <norman.enriquez@gmail.com>
2015-07-26 22:54:22 +0800

    Update cachematrix.R
    Assignment 2 - R Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
 ## The 2 function will cache the inverse of the matrix
 
 ## This function will create listing contains in a function
+## - set and get the value of the matrix
+## - set and get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
     inverseMatrix <- NULL
@@ -18,7 +20,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## This function will return an inverse of the matrix 
+## This function will return an inverse of the matrix. It will check if the inverse matrix has been computer.
+## If it's computed, it will get the result and skips the computation. If it's not computed, it will sets the value in the
+## cache thru setinvere function.
 
 cacheSolve <- function(a, ...) {
     inverseMatrix <- x$getinverse()
